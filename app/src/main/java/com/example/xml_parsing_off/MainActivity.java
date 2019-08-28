dfffd93f3b5f2f7ca5d5e0aa6c87d6b67510abfb
batteashishkumar <ashishakash55@gmail.com>
2019-08-28 15:20:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/xml_parsing_off/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.xml_parsing_off;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv1=(TextView)findViewById(R.id.textView1);
+        try {
+
+            InputStream is = getAssets().open("file.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(is);
+
+            Element element=doc.getDocumentElement();
+            element.normalize();
+
+            NodeList nList = doc.getElementsByTagName("employee");
+
+            for (int i=0; i<nList.getLength(); i++) {
+
+                Node node = nList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element2 = (Element) node;
+                    tv1.setText(tv1.getText()+"\nName : " + getValue("name", element2)+"\n");
+                    tv1.setText(tv1.getText()+"Description : " + getValue("surname", element2)+"\n");
+                    tv1.setText(tv1.getText()+"-----------------------");
+                }
+            }
+
+        } catch (Exception e) {e.printStackTrace();}
+
+    }
+
+    private static String getValue(String tag, Element element) {
+        NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
+        Node node = nodeList.item(0);
+        return node.getNodeValue();
+    }
+
+}
+
